54d2a894a097517a7de630e223f283fd635c6bc5
stdgregwar <gregoirehirt@gmail.com>
2015-10-23 16:05:32 +0200

    Mat4 static methods.

--- include/Mat4.h
+++ include/Mat4.h
@@ -10,22 +10,32 @@
 class Mat4
 {
 public:
+    enum Axes
+    {
+        X_AXIS,
+        Y_AXIS,
+        Z_AXIS
+    };
     Mat4();
     Mat4(const Mat4& other);
     Mat4(Mat4&& other);
-    Mat4 operator*(const Mat4& other);
     Mat4& operator=(const Mat4& other);
-    Mat4& operator=(const Mat4&& other);
+    Mat4& operator=(Mat4&& other);
     float* operator[](std::size_t i);
+    float* operator[](std::size_t i) const;
+    Mat4 operator*(const Mat4& other) const;
     Mat4& operator*=(const Mat4& other);
     Mat4& translate(const sf::Vector3f d);
     Mat4& translate(float x, float y, float z);
     Mat4& rotate(float rx, float ry, float rz);
-    Mat4& rotate(const sf::Vector3f& axis, float angle);
+    Mat4& rotate(Axes axe, float angle);
     Mat4& scale(const sf::Vector3f& scale);
     Mat4& scale(float x, float y, float z);
     static Mat4 perpective(float ratio, float angle, float near, float far);
-    static Mat4 orthographic(const sf::Vector3f& first, const sf::Vector3f& second);
+    static Mat4 orthographic(float x1, float x2, float y1, float y2, float z1, float z2);
+    static Mat4 translation(const sf::Vector3f& r);
+    static Mat4 scaling(const sf::Vector3f &scale);
+    static Mat4 rotation(Axes axe,float angle);
     ~Mat4();
 private:
     float* mElements;
--- src/Mat4.cpp
+++ src/Mat4.cpp
@@ -1,8 +1,15 @@
 #include "../include/Mat4.h"
+#include <cstring>
+#include <cmath>
+
+using namespace std;
 
 Mat4::Mat4()
 {
     mElements = new float[16];
+    for(int i = 0; i < 4; i++)
+        for(int j = 0; i < 4; i++)
+            (*this)[i][j] = 0;
 }
 
 Mat4::Mat4(const Mat4& other) : Mat4()
@@ -30,7 +37,7 @@ Mat4& Mat4::operator=(Mat4&& other)
     return *this;
 }
 
-Mat4 Mat4::operator*(const Mat4& b)
+Mat4 Mat4::operator*(const Mat4& b) const
 {
     const Mat4& a(*this);
     Mat4 r;
@@ -46,6 +53,11 @@ float* Mat4::operator[](std::size_t i)
     return &mElements[i*4];
 }
 
+float* Mat4::operator[](std::size_t i) const
+{
+    return &mElements[i*4];
+}
+
 Mat4& Mat4::operator*=(const Mat4& other)
 {
     Mat4 r = other*(*this);
@@ -57,4 +69,53 @@ Mat4::~Mat4()
 {
     if(mElements)
         delete mElements;
+}
+
+Mat4 perpective(float ratio, float angle, float near, float far)
+{
+    constexpr float TO_RADIANS = 3.1415 / 180;
+    float zz = far-near;
+    Mat4 mat;
+    mat[0][0] = 1/tan(angle*TO_RADIANS);
+    mat[1][1] = ratio/tan(angle*TO_RADIANS);
+    mat[2][2] = (near+far)/zz;
+    mat[2][3] = -2*near*far/zz;
+    mat[3][2] = 1;
+    return mat;
+}
+
+Mat4 orthographic(const sf::Vector3f& first, const sf::Vector3f& second)
+{
+    //TEMP
+    return Mat4();
+}
+
+Mat4 Mat4::translation(const sf::Vector3f& r)
+{
+    Mat4 m;
+    m[0][3] = r.x;
+    m[1][3] = r.y;
+    m[2][3] = r.z;
+    return m;
+}
+
+Mat4 Mat4::scaling(const sf::Vector3f &s)
+{
+    Mat4 m;
+    m[0][0] = s.x;
+    m[1][1] = s.y;
+    m[2][2] = s.z;
+    m[3][3] = 1;
+    return m;
+}
+
+
+Mat4 Mat4::rotation(Axes axe,float angle)
+{
+    Mat4 m;
+    switch(axe)
+    {
+        case X_AXIS:
+            break;
+    };
 }
