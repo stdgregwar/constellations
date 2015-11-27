f9495b30f205bb890e96131cdf798080bfcba93f
stdgregwar <gregoirehirt@gmail.com>
2015-11-27 15:50:08 +0100

    Transition abstract

--- src/Transition.cpp
+++ src/Transition.cpp
@@ -4,3 +4,16 @@ Transition::Transition()
 {
 
 }
+<<<<<<< HEAD
+=======
+
+void Transition::setTexture(sf::Texture* tex)
+{
+    mTexture = tex;
+}
+
+const sf::Texture* Transition::tRender() const
+{
+    return mTex;
+}
+>>>>>>> Transition abstract
