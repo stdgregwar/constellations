2e7ca43a2553e122b54c813ec1af727e8b7e5d4d
stdgregwar <gregoirehirt@gmail.com>
2015-11-27 15:50:08 +0100

    Transition abstract

---
+++ src/Transition.cpp
@@ -0,0 +1,16 @@
+#include "../include/Transition.h"
+
+Transition::Transition()
+{
+
+}
+
+void Transition::setTexture(sf::Texture* tex)
+{
+    mTexture = tex;
+}
+
+const sf::Texture* Transition::tRender() const
+{
+    return mTex;
+}
